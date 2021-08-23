4ca256a7b18d8a713aa63abd36558ea32117b4a9
Radensuaso <andrels9283@gmail.com>
2021-08-23 14:47:32 +0100

    Inserted the Authors, in the process of introducing books

---
+++ 2-insert-into.sql
@@ -0,0 +1,25 @@
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('Dan','Brown',1964,'USA');
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('Agatha','Christie',1890,'UK');
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('Paulo','Coelho',1947,'Brazil');
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('Ken','Follett',1949,'UK');
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('John','Grisham',1955,'USA');
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('Stephen','King',1947,'USA');
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('Haruki','Murakami',1949,'Japan');
+INSERT INTO authors(name,last_name,birth_year,country) VALUES('James','Patterson',1947,'USA');
+
+INSERT INTO books(name,category,cover,author,published_at) VALUES('Da Vinci Code','mystery thriller','https://images-na.ssl-images-amazon.com/images/I/61voOxiNf9L.jpg','Dan Brown','2003-03-18');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('Angels and Demons','mystery thriller','https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQj8XUReijor_2XAJnJ26i1_-J8bxbnEWXE6CYTOuEwjczu8wSl','Dan Brown','2000-05-01');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('The Lost Symbol','mystery thriller','https://images-na.ssl-images-amazon.com/images/I/71X1p4TGlxL.jpg','Dan Brown','2009-09-15');
+
+INSERT INTO books(name,category,cover,author,published_at) VALUES('Murder on the Orient Express','Crime','https://images-na.ssl-images-amazon.com/images/I/51H9A6DHe3S._SX324_BO1,204,203,200_.jpg','Agatha Christie','1934-01-01');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('The Mysterious Affair at Styles','Crime','https://images-na.ssl-images-amazon.com/images/I/918TvCMQO4L.jpg','Agatha Christie','1920-10-01');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('Death on the Nile','Crime','https://images-na.ssl-images-amazon.com/images/I/71Hm+U6EWgL.jpg','Agatha Christie','1937-11-01');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('Agatha Christies Poirot','Crime','https://m.media-amazon.com/images/M/MV5BODljYzNlZTMtMWIwNi00N2QzLWE2ZTAtZDBmYzIwYzhlZDY2XkEyXkFqcGdeQXVyMjExMjk0ODk@._V1_.jpg','Agatha Christie','1989-01-08');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('The Murder of Roger Ackroyd','Crime','https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcQ_GSPt6WQXmrGO17mh01OC5WXMKZY1kHF2uCRlXVgkyLUQQGKG','Agatha Christie','1926-06-01');
+
+INSERT INTO books(name,category,cover,author,published_at) VALUES('The Alchemist','Quest','https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcR0tgvYN4QHjHQDige5hIX2HkIe5hLSgwDr5zrn2Vd1-bchhyIM','Paulo Coelho','1988-01-01');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('Veronika Decides to Die','Quest','https://i.gr-assets.com/images/S/compressed.photo.goodreads.com/books/1348139939l/1431.jpg','Paulo Coelho','1998-01-01');
+INSERT INTO books(name,category,cover,author,published_at) VALUES('Manual of the Warrior of Light','Quest','https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcSApELMTg6SEtoH-AohZrAYSrKfjBTZf_Z2ThKKTwvPgwkGGbUX','Paulo Coelho','1997-01-01');
+
+
+
